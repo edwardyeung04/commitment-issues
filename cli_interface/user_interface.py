0a2b56962bbb02fd9332f9e5737b1692c4ddaf4f
edwardyeung04 <edwardbuddyboy@gmail.com>
2024-12-07 19:50:50 -0800

    refactor | user interface: Clear existing commit message before prompting for change type

--- cli_interface/user_interface.py
+++ cli_interface/user_interface.py
@@ -57,6 +57,7 @@ class UserInterface:
         return input("Please provide feedback for regeneration (or press Enter to skip): ")
 
     def prompt_manual_edit(self, existing_commit_message):
+        existing_commit_message = ""
         # Ask user for change type
         change_type = self.prompt_change_type()
 
